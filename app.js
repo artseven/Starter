c05b6faffba89aac55a2c41a6a912bae85f93349
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-09 15:56:25 -0500

    injecting array of things

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($log, $scope, $filter, $resource) {
-
+// myApp.controller('mainController', function($log, $scope, $filter, $resource) {
+   myApp.controller('mainController', ['$scope', '$log','$filter','$resource', function($scope, $log, $filter, $resource) {
+        
     console.log($scope);
     console.log($log);
     console.log($resource);
@@ -19,4 +20,4 @@ myApp.controller('mainController', function($log, $scope, $filter, $resource) {
 
 
 
-});
+}]);
