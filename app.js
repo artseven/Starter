6075ec990620838474b4b7ac1ba7f87b0158e0ee
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-09 15:29:33 -0500

    upd

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages']);
 
-myApp.controller('mainController', function($scope, $log) {
-    console.log($log);
+myApp.controller('mainController', function($log, $scope, $filter) {
+
     console.log($scope);
+    console.log($log);
+
+    $log.log('Hello.');
+    $log.info('This is some information');
+    $log.warn('Warning!');
+    $log.debug('Some debug information');
+    $log.error('This was an error!');
+
+    $scope.name = 'John';
+    $scope.formattedname = $filter('uppercase')($scope.name)
+
+    $log.log($scope.formattedname);
+
+    
+
 });
