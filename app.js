8fc90b6655f1de506229321ff0e0d2a4907deffe
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-15 12:16:01 -0500

    directive as attribute and element

--- app.js
+++ app.js
@@ -133,6 +133,11 @@ myApp.controller('secondController', [
 
 myApp.directive("searchResults", function() {
     return {
-        template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>'
+        //      atribute      and  element can be used
+        // <div search-results>   <search-results> 
+        restrict: 'AE',
+        template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>',
+        // to delete <search-results></search-results> from DOM
+        replace: true
     };
 });
